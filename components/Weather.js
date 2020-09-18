1ee66cc1786cd2978bac8c3c092d72f37b220841
Warodom Werapun <wwarodom@gmail.com>
2020-09-18 11:32:35 +0700

    add fetch API

--- components/Weather.js
+++ components/Weather.js
@@ -1,6 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, ImageBackground, StyleSheet } from 'react-native';
 import Forecast from './Forecast';
+
+
+const apiKey = '6cecb9dd2e369d7e9b5d62bc682150d4'
 
 export default function Weather(props) {
     const [forecastInfo, setForecastInfo] = useState({
@@ -8,6 +11,26 @@ export default function Weather(props) {
         description: 'description',
         temp: 0
     })
+
+    useEffect(() => {
+        console.log(`fetching data with zipCode = ${props.zipCode}`)
+        if (props.zipCode) {
+            fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=${apiKey}`)
+                .then((response) => response.json())
+                .then((json) => {
+                    setForecastInfo({
+                        main: json.weather[0].main,
+                        description: json.weather[0].description,
+                        temp: json.main.temp
+                    });
+                })
+                .catch((error) => {
+                    console.warn(error);
+                });
+        }
+    }, [props.zipCode])
+
+
     return (
         <View>
             <ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
@@ -22,12 +45,12 @@ export default function Weather(props) {
 const styles = StyleSheet.create({
     backdrop: {
         alignItems: 'center',
-        
+
         width: '100%',
         height: '100%'
     },
     cover: {
-        backgroundColor: 'black',        
+        backgroundColor: 'black',
         width: '100%',
         height: 250,
         opacity: 0.4,
